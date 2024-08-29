d5be3b98c20b80e35da8d904dd2299cd2620a4e4
nagasai99797 <nagasaideep.puppala@sveps.co.in>
2024-08-29 03:15:03 -0500

    setup

---
+++ src/main/java/in/co/sveps/sveps_core_notification/SvepsCoreNotificationApplication.java
@@ -0,0 +1,13 @@
+package in.co.sveps.sveps_core_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvepsCoreNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvepsCoreNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/co/sveps/sveps_core_notification/SvepsCoreNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.sveps.sveps_core_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvepsCoreNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
